2a350824b1eb3dfe65dd7637ef627c1ebe6cb242
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-09-17 17:44:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = int(os.environ.get("API_ID", "33029767"))
 API_HASH = os.environ.get("API_HASH", "5d897bed11bc8b062a12f6c1c3c5360a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8986691330:AAHqk29Id7Rxz_7ErbJeli4uhnOMPrUcH0g")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8756360185:AAGW1mGND__G40jV1dxZNSNwb8k89fMOWoI")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8931907813"))
 
